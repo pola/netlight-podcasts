acd7282af7cea7c584c62d7584f55afea1fecbcb
Martin Pola <martin.pola@netlight.com>
2021-04-24 17:38:25 +0200

    updated database structure

--- backend/structure.sql
+++ backend/structure.sql
@@ -1,7 +1,3 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
 CREATE TABLE `account` (
   `id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -9,6 +5,32 @@ CREATE TABLE `account` (
   `timestampSeen` bigint(30) NOT NULL,
   `isAdmin` tinyint(1) NOT NULL,
   `isRemoved` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `logPodcastTokenAudio` (
+  `id` int(11) NOT NULL,
+  `timestamp` bigint(30) NOT NULL,
+  `ip` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `userAgent` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `episode` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `logPodcastTokenRss` (
+  `id` int(11) NOT NULL,
+  `timestamp` bigint(30) NOT NULL,
+  `ip` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `userAgent` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `logPodcastVisit` (
+  `id` int(11) NOT NULL,
+  `timestamp` bigint(30) NOT NULL,
+  `ip` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `userAgent` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `account` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `podcast` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `podcast` (
@@ -45,6 +67,20 @@ CREATE TABLE `podcastToken` (
 ALTER TABLE `account`
   ADD PRIMARY KEY (`id`);
 
+ALTER TABLE `logPodcastTokenAudio`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `token` (`token`),
+  ADD KEY `episode` (`episode`);
+
+ALTER TABLE `logPodcastTokenRss`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `token` (`token`);
+
+ALTER TABLE `logPodcastVisit`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `account` (`account`),
+  ADD KEY `podcast` (`podcast`);
+
 ALTER TABLE `podcast`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `slug` (`slug`);
@@ -55,12 +91,35 @@ ALTER TABLE `podcastEpisode`
 
 ALTER TABLE `podcastToken`
   ADD PRIMARY KEY (`token`),
-  ADD KEY `account` (`account`);
+  ADD KEY `account` (`account`),
+  ADD KEY `podcast` (`podcast`);
 
+
+ALTER TABLE `logPodcastTokenAudio`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `logPodcastTokenRss`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `logPodcastVisit`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `logPodcastTokenAudio`
+  ADD CONSTRAINT `logPodcastTokenAudio_ibfk_2` FOREIGN KEY (`token`) REFERENCES `podcastToken` (`token`),
+  ADD CONSTRAINT `logPodcastTokenAudio_ibfk_3` FOREIGN KEY (`episode`) REFERENCES `podcastEpisode` (`id`);
+
+ALTER TABLE `logPodcastTokenRss`
+  ADD CONSTRAINT `logPodcastTokenRss_ibfk_2` FOREIGN KEY (`token`) REFERENCES `podcastToken` (`token`);
+
+ALTER TABLE `logPodcastVisit`
+  ADD CONSTRAINT `logPodcastVisit_ibfk_1` FOREIGN KEY (`account`) REFERENCES `account` (`id`),
+  ADD CONSTRAINT `logPodcastVisit_ibfk_2` FOREIGN KEY (`podcast`) REFERENCES `podcast` (`id`);
 
 ALTER TABLE `podcastEpisode`
   ADD CONSTRAINT `podcastEpisode_ibfk_1` FOREIGN KEY (`podcast`) REFERENCES `podcast` (`id`);
 
 ALTER TABLE `podcastToken`
-  ADD CONSTRAINT `podcastToken_ibfk_1` FOREIGN KEY (`account`) REFERENCES `account` (`id`);
+  ADD CONSTRAINT `podcastToken_ibfk_1` FOREIGN KEY (`account`) REFERENCES `account` (`id`),
+  ADD CONSTRAINT `podcastToken_ibfk_2` FOREIGN KEY (`podcast`) REFERENCES `podcast` (`id`);
 COMMIT;
