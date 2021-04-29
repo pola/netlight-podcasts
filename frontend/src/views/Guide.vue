b6e144cfdf80a76254fe73ab7efed4036c145f07
Martin Pola <martin.pola@netlight.com>
2021-04-29 22:14:14 +0200

    let external links open in new tabs etc.

--- frontend/src/views/Guide.vue
+++ frontend/src/views/Guide.vue
@@ -1,19 +1,19 @@
 <template>
-<div>
+  <div>
     <h2>How to add an RSS feed to your application</h2>
-    <p>On this site you can view a list over which applications lets you listen to a private RSS feed: <a href="https://support.patreon.com/hc/en-us/articles/115005429546-Which-podcast-apps-will-my-custom-audio-RSS-link-work-with-">Vistit link!</a></p>
+    <p>
+      On this site you can view a list over which applications lets you listen to a private RSS feed: <a
+        href="https://support.patreon.com/hc/en-us/articles/115005429546-Which-podcast-apps-will-my-custom-audio-RSS-link-work-with-"
+        target="_blank"
+      >Visit link!</a>
+    </p>
   
-    <p>Here is a guide on how to add it to the Apple podcast app: <a href="https://support.patreon.com/hc/en-us/articles/115000877506-Add-my-private-RSS-feed-to-the-Apple-Podcast-app">Visit link!</a></p>
+    <p>
+      Here is a guide on how to add it to the Apple podcast app: <a
+        href="https://support.patreon.com/hc/en-us/articles/115000877506-Add-my-private-RSS-feed-to-the-Apple-Podcast-app"
+        target="_blank"
+      >Visit link!</a>
+    </p>
     <p>Unfortunately Spotify does not yet support private RSS feeds. Maybe one  of our consultants there can fix this? :D </p>
-</div>
+  </div>
 </template>
-
-<script>
-
-export default {
-}
-</script>
-
-<style land="scss" scoped>
-
-</style>
